14deb004d7a468096a77a2a90854847b57e304e9
sreesayanth <sree@dataguardnxt.com>
2025-03-14 17:27:18 +0530

    feat: add global exception handling with AllExceptionsFilter and integrate it into the application

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './modules/common/filters/http-exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('nex-projects API')
@@ -32,8 +35,9 @@ async function bootstrap() {
 
   // Enable ValidationPipe for request validation
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
 
+  app.enableCors();
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`🚀 Server started on port ${process.env.PORT || 3000} `, 'Bootstrap');
 }
