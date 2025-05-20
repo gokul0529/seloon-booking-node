315e59258c46ba16ec9835da12e371f23a5408b9
sreesayanth <yournewemail@example.com>
2025-05-20 16:24:36 +0530

    feat: implement ticket module with controller, service, and DTOs for ticket management

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { EmailIntegrationModule } from './modules/email-integration/email-integration.module';
 import { EmailAuthModule } from './modules/email-auth/email-auth.module';
+import { TicketModule } from './modules/ticket/ticket.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { EmailAuthModule } from './modules/email-auth/email-auth.module';
     AuthModule,
     UsersModule,
     EmailIntegrationModule,
-    EmailAuthModule
+    EmailAuthModule,
+    TicketModule
   ],
   controllers: [AppController],
   providers: [AppService],
